c1fa843bae1739f9d09b4001cfa0a571c2bffad2
Jhxnnat <165497710+Jhxnnat@users.noreply.github.com>
2024-04-19 17:53:57 -0500

    logout fix

--- inventory/urls.py
+++ inventory/urls.py
@@ -13,3 +13,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='inventory/logout.html'), name='logout'),
 ]
+
